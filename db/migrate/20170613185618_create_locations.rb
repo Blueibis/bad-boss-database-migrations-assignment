304df07cd0fb5fab698e8770fe2af5505407c655
Blueibis <jennie.li.2011@gmail.com>
2017-06-13 14:58:18 -0400

    adddddded tabless

---
+++ db/migrate/20170613185618_create_locations.rb
@@ -0,0 +1,10 @@
+class CreateLocations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.string :city
+      t.string :street
+      t.integer :address
+    end
+  end
+end
